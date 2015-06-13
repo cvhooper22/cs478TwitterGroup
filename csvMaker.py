b539c3792625220c2387c91c9f008d3242760654
Matt Freeman <matt.mission.mail22@gmail.com>
2015-06-13 01:55:47 -0600

    added script to make a csv file as well as first try at a distanceMatrixMaker from the csv produced

---
+++ csvMaker.py
@@ -0,0 +1,399 @@
+__author__ = 'Matthew'
+import os
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+from decimal import *
+import time
+import string
+import regex as re
+
+
+
+def clean_tweet_text(tweet_text):
+    tweet_text = tweet_text.lower()
+    tweet_text = re.sub(ur"\p{P}+", "", tweet_text)
+    tweet_text = re.sub("[^a-zA-Z\s]","", tweet_text)
+    tweet_text = filter(lambda x: x in string.printable, tweet_text)
+    tweet_text.encode('ascii',errors='ignore')
+    return tweet_text
+
+def remove_single_quotes(in_string):
+    utfstring = in_string.encode('utf-8', 'ignore')
+    # some things have returns(\r)m (\n), and(\\) in them, get rid of those b/c weka hates them in the dataset
+
+    in_string = in_string.strip("\\")
+
+    slash_split = in_string.split("\\")
+    if len(slash_split) == 1:
+        pass
+    else:
+        corrected_string = ""
+        for x in range(0, len(slash_split) - 1):
+            corrected_string += slash_split[x]
+        if slash_split[len(slash_split) - 1] == '':
+            pass
+        else:
+            corrected_string += slash_split[len(slash_split) - 1]
+        in_string = corrected_string
+
+    in_string = in_string.strip("\r")
+
+    return_split = in_string.split("\r")
+    if len(return_split) == 1:
+        pass
+    else:
+        fixed_string = ""
+        for x in range(0, len(return_split) - 1):
+            fixed_string += return_split[x]
+        if return_split[len(return_split) - 1] == '':
+            pass
+        else:
+            fixed_string += return_split[len(return_split) - 1]
+        in_string = fixed_string
+
+    in_string = in_string.strip("\n")
+    newline_split = in_string.split("\n")
+    if len(newline_split) == 1:
+        pass
+    else:
+        better_string = ""
+        for x in range(0, len(newline_split) - 1):
+            better_string += newline_split[x]
+        if newline_split[len(newline_split) - 1] == '':
+            pass
+        else:
+            better_string += newline_split[len(newline_split) - 1]
+        in_string = better_string
+
+    in_string = in_string.strip("\'")
+    split_on_quotes = in_string.split("\'")
+    if len(split_on_quotes) == 1:
+        return in_string
+    else:
+        returner = ""
+        for x in range(0, len(split_on_quotes) - 1):
+            returner += split_on_quotes[x]
+        if split_on_quotes[len(split_on_quotes) - 1] == '':
+            return returner
+        else:
+            returner += split_on_quotes[len(split_on_quotes) - 1]
+        return returner
+
+
+def write_att_val(val):
+    val = val + ","
+    in_string = val.encode('utf-8', 'ignore')
+    output_file.write(in_string)
+
+
+def count_words(this_tweet):
+    all_words = this_tweet.split()
+    length = len(all_words)
+    return length
+
+
+def count_keywords(this_tweet):
+    this_tweet = clean_tweet_text(this_tweet)
+    tweet_words = this_tweet.split()
+    count = 0
+    for word in tweet_words:
+        for keyword in keywords:
+            lower_word = word.lower()
+            lower_keyword = keyword.lower()
+            if lower_keyword != '' and lower_keyword == lower_word:
+                count += 1
+    return count
+
+
+def find_if_replied(reply_user_id):
+    if reply_user_id is None:
+        return False
+    else:
+        return True
+
+
+def get_string_time(time):
+    if time > 21.165 or time < 4.9834:
+        return 'late'
+    elif 4.9832 < time < 11.01:
+        return 'morning'
+    elif 11.0 < time < 16.9834:
+        return 'afternoon'
+    elif 16.9832 < time < 21.01:
+        return 'evening'
+
+
+def calc_num_time(date_string):
+    split_vals = date_string.split(" ")
+    time_string = split_vals[3]
+    time_vals = time_string.split(":")
+    hour = Decimal(time_vals[0])
+    minute_num = Decimal(time_vals[1])
+    minute = minute_num / 60
+    time = hour + minute
+    return time
+
+
+def find_common_time(late_ct, morning_ct, afternoon_ct, evening_ct):
+    top_ct = max(late_ct, morning_ct, afternoon_ct, evening_ct)
+    if late_ct == top_ct:
+        return 'late'
+    elif morning_ct == top_ct:
+        return 'morning'
+    elif afternoon_ct == top_ct:
+        return 'afternoon'
+    elif evening_ct == top_ct:
+        return 'evening'
+
+
+def calc_age(date_string):
+    time_vals = date_string.split(" ")
+    year = Decimal(time_vals[5])
+    month_string = time_vals[1]
+    month = get_month_from_string(month_string)
+    month_diff = month - 6
+    year_diff = 2015 - year
+    if month_diff <= 0:
+        year_diff += Decimal(abs(month_diff)) / 12
+    else:
+        actual_diff = 12 - month_diff
+        year_diff += Decimal(actual_diff) / 12
+    return year_diff
+
+
+def get_month_from_string(in_string):
+    if in_string == 'Jan':
+        return 1
+    elif in_string == 'Feb':
+        return 2
+    elif in_string == 'Mar':
+        return 3
+    elif in_string == 'Apr':
+        return 4
+    elif in_string == 'May':
+        return 5
+    elif in_string == 'Jun':
+        return 6
+    elif in_string == 'Jul':
+        return 7
+    elif in_string == 'Aug':
+        return 8
+    elif in_string == 'Sep':
+        return 9
+    elif in_string == 'Oct':
+        return 10
+    elif in_string == 'Nov':
+        return 11
+    elif in_string == 'Dec':
+        return 12
+
+def add_keywords(tweet_text):
+    tweet_text = clean_tweet_text(tweet_text)
+    words_in_tweet = tweet_text.split()
+    for word in words_in_tweet:
+        if (word != '') and (word not in stopwords) and ('http' not in word):
+            keywords.add(word)
+
+
+input_data_path = 'users\\'
+output_file_name = 'twitter_users_12.csv'
+
+filenames = os.listdir("users")
+
+#TODO: REPLACE KEYWORDS_ARRAY WITH USER-SPECIFIC KEYWORDS)
+
+#keywords_array = []
+# load the keywords to use in computation later
+#with open("keywords.txt", "r") as keyword_file:
+    #my_words = keyword_file.read()
+    #keywords_array = my_words.split()
+
+stopwords = []
+with open("stopwords.txt", "r") as stopwords_file:
+    stopwords_text = stopwords_file.read()
+    stopwords_text = re.sub(ur"\p{P}+", "", stopwords_text)
+    stopwords = stopwords_text.lower().split()
+    
+
+# This is created here to make it globally accessible, however it is emptied for each user.
+keywords = set()
+# first thing to write is the headers from the other file
+# headerFile = open('csvHeader.txt', "r")
+# contents = headerFile.read()
+# contents = contents.encode('utf-8', 'ignore')
+output_file = open(output_file_name, "a")
+# output_file.write(contents)
+
+# these next calls are for the DECIMAL package and make it so that all decimals are rounded UP to the
+# fourth decimal place
+getcontext().prec = 4
+getcontext().rounding = ROUND_UP
+
+fileindex = 0
+# start_time = time.clock()
+for filename in filenames:
+    fileindex += 1
+    print "Working on file {index} of {total}".format(index=fileindex, total= len(filenames))
+    # time_now = time.clock()
+    # time_so_far = time_now - start_time
+    # print "Time so far: {}".format(time_so_far)
+    tweets_data = []
+    input_path = input_data_path + filename
+    print input_path
+    tweets_file = open(input_path, "r")
+    for line in tweets_file:
+        try:
+            tweet = json.loads(line)
+            tweets_data.append(tweet)
+        except:
+            continue
+
+    # set up user values
+    keywords = set()
+    num_tweets = Decimal(len(tweets_data))
+    retweeted_count = 0
+    favorited_count = 0
+    avg_wordcount = 0
+    avg_hashcount = 0
+    avg_keywords = 0
+    avg_links = 0
+    avg_mentions = 0
+    avg_symbols = 0
+    num_replies = 0
+    media_count = 0
+    # these next four variables reference the different times of day that someone's tweets can show up
+    # late --> 11:01pm-4:59am
+    # morning --> 5:00am-11:00am
+    # afternoon --> 11:01am-4:59pm
+    # evening --> 5:00pm-11:00pm
+    late_count = 0
+    morning_count = 0
+    afternoon_count = 0
+    evening_count = 0
+
+    for x in range(len(tweets_data)):
+        tweet = tweets_data[x]
+        tweet_text = tweet['text']
+        
+        add_keywords(tweet_text)
+        wordcount = count_words(tweet_text)
+        avg_wordcount += wordcount
+
+        keyword_count = count_keywords(tweet_text)
+        avg_keywords += keyword_count
+
+        hashtag_count = len(tweet['entities']['hashtags'])
+        avg_hashcount += hashtag_count
+
+        in_reply_to = find_if_replied(tweet['in_reply_to_user_id'])
+        if in_reply_to:
+            num_replies += 1
+
+        urls = tweet['entities']['urls']
+        num_links = len(urls)
+        avg_links += num_links
+
+        mentions = len(tweet['entities']['user_mentions'])
+        avg_mentions += mentions
+
+        num_symbols = len(tweet['entities']['symbols'])
+        avg_symbols += num_symbols
+
+        time_string = tweet['created_at']
+        time_num = calc_num_time(time_string)
+        time_of_day = get_string_time(time_num)
+        if time_of_day == 'late':
+            late_count += 1
+        elif time_of_day == 'morning':
+            morning_count += 1
+        elif time_of_day == 'afternoon':
+            afternoon_count += 1
+        elif time_of_day == 'evening':
+            evening_count += 1
+
+        retweet_count = tweet['retweet_count']
+        retweeted_count += retweet_count
+
+        tweet_favorite_count = tweet['favorite_count']
+        favorited_count += tweet_favorite_count
+
+        try:
+            media = tweet['entities']['media']
+            if media is not None:
+                has_media = len(tweet['entities']['media'])
+                if has_media > 0:
+                    media_count += 1
+        except:
+            pass
+
+        if x == len(tweets_data) - 1:
+            # TODO here is where we write the attributes
+            keyword_csv_string = ' '.join(keywords)
+            write_att_val("\'" + keyword_csv_string + "\'")
+            
+            wordcount = avg_wordcount / num_tweets
+            write_att_val(str(wordcount))
+
+            keyword_count = avg_keywords / num_tweets
+            write_att_val(str(keyword_count))
+
+            hashtag_count = avg_hashcount / num_tweets
+            write_att_val(str(hashtag_count))
+
+            in_reply_to = num_replies / num_tweets
+            write_att_val(str(in_reply_to))
+
+            username = tweet['user']['name']
+            write_att_val('\'' + remove_single_quotes(username) + '\'')
+
+            screen_name = tweet['user']['screen_name']
+            write_att_val('\'' + remove_single_quotes(screen_name) + '\'')
+
+            user_url = tweet['user']['url']
+            if user_url is None:
+                write_att_val('\'null\'')
+            else:
+                write_att_val('\'' + remove_single_quotes(user_url) + '\'')
+
+            verification = tweet['user']['verified']
+            write_att_val(str(verification))
+
+            follower_count = tweet['user']['followers_count']
+            write_att_val(str(follower_count))
+
+            user_favorited_count = tweet['user']['favourites_count']
+            write_att_val(str(user_favorited_count))
+
+            tweet_count = tweet['user']['statuses_count']
+            write_att_val(str(tweet_count))
+
+            num_links = avg_links / num_tweets
+            write_att_val(str(num_links))
+
+            mentions = avg_mentions / num_tweets
+            write_att_val(str(mentions))
+
+            num_symbols = avg_symbols / num_tweets
+            write_att_val(str(num_symbols))
+
+            time = find_common_time(late_count, morning_count, afternoon_count, evening_count)
+            write_att_val(time)
+
+            user_age = calc_age(tweet['user']['created_at'])
+            write_att_val(str(user_age))
+
+            retweet_count = retweeted_count
+            write_att_val(str(retweet_count))
+
+            been_favorite_count = favorited_count
+            write_att_val(str(been_favorite_count))
+
+            media_freq = media_count / num_tweets
+            last_att = str(media_freq) + "\n"
+            last_att = last_att.encode('utf-8', 'ignore')
+            output_file.write(last_att)
+
+print "Done"
+
+
---
+++ distanceMatrixMaker.py
@@ -0,0 +1,167 @@
+import numpy
+import csv
+from decimal import *
+from scipy.spatial import distance
+
+#define the weights for the different attribute distances
+keywords_weight = Decimal(1.0)
+wordcount_weight = Decimal(0.4)
+num_key_words_weight = Decimal(0.2)
+num_hash_weight = Decimal(0.5)
+if_in_reply_weight = Decimal(0.2)
+username_weight = Decimal(0.0)
+screen_name_weight = Decimal(0.0)
+url_weight = Decimal(0.0)
+verified_weight = Decimal(0.0)
+followers_count_weight = Decimal(0.5)
+favorites_count_weight = Decimal(0.6)
+tweet_count_weight = Decimal(0.3)
+num_links_weight = Decimal(0.1)
+user_mentions_weight = Decimal(0.1)
+symbols_used_weight = Decimal(0.3)
+time_weight = Decimal(0.7)
+user_age_weight = Decimal(0.3)
+retweet_count_weight = Decimal(0.6)
+favorited_count_weight = Decimal(0.6)
+media_freq_weight = Decimal(0.4)
+
+def compute_distance(instance1, instance2):
+    total_distance = 0
+
+    #
+    total_distance += keywords_weight * compute_keyword_distance(instance1[0], instance2[0])
+
+    total_distance += wordcount_weight * euclidean(instance1[1], instance2[1])
+
+    total_distance += num_key_words_weight * euclidean(instance1[2], instance2[2])
+
+    total_distance += num_hash_weight * euclidean(instance1[3], instance2[3])
+
+    total_distance += if_in_reply_weight * euclidean(instance1[4], instance2[4])
+
+    total_distance += username_weight * compute_string_distance(instance1[5], instance2[5])
+
+    total_distance += screen_name_weight * compute_string_distance(instance1[6], instance2[6])
+
+    total_distance += url_weight * compute_string_distance(instance1[7], instance2[7])
+
+    total_distance += verified_weight * compute_verified_distance(instance1[8], instance2[8])
+
+    total_distance += followers_count_weight * euclidean(instance1[9], instance2[9])
+
+    total_distance += favorited_count_weight * euclidean(instance1[10], instance2[10])
+
+    total_distance += tweet_count_weight * euclidean(instance1[11], instance2[11])
+
+    total_distance += num_links_weight * euclidean(instance1[12], instance2[12])
+
+    total_distance += user_mentions_weight * euclidean(instance1[13], instance2[13])
+
+    total_distance += symbols_used_weight * euclidean(instance1[14], instance2[14])
+
+    total_distance += time_weight * compute_time_distance(instance1[15], instance2[15])
+
+    total_distance += user_age_weight * euclidean(instance1[16], instance2[16])
+
+    total_distance += retweet_count_weight * euclidean(instance1[17], instance2[17])
+
+    total_distance += favorited_count_weight * euclidean(instance1[18], instance2[18])
+
+    total_distance += media_freq_weight * euclidean(instance1[19], instance2[19])
+
+    return total_distance
+
+
+def compute_keyword_distance(tweets1, tweets2):
+    # currently computes distance by finding words that are the same and then returning 1/ num shared words
+    # if there are no matching words, returns a distance of 2
+    shared_words = 0.0
+
+    pure_string1 = tweets1.strip('\'')
+    pure_string2 = tweets2.strip('\'')
+
+    words_array1 = pure_string1.split(" ")
+    words_array2 = pure_string2.split(" ")
+
+    smaller_range = min(len(words_array1), len(words_array2))
+    larger_range = max(len(words_array1), len(words_array2))
+
+    smaller_array = []
+    larger_array = []
+
+    if smaller_range == len(words_array1):
+        smaller_array = words_array1
+        larger_array = words_array2
+    else:
+        larger_array = words_array1
+        smaller_array = words_array2
+
+    for x in range(smaller_range):
+        for y in range(larger_range):
+            if smaller_array[x] == larger_array[y]:
+                shared_words += 1.0
+
+    if shared_words != 0.0:
+        return Decimal(1.0 / shared_words)
+    else:
+        return Decimal(2.0)
+
+def compute_string_distance(string1, string2):
+    return abs(len(string2) - len(string1))
+
+def compute_verified_distance(verif1, verif2):
+    if verif1 == 'True' and verif2 == 'True':
+        return 1
+    elif verif1 == 'False' and verif2 == 'False':
+        return 1
+    else:
+        return 0
+
+def compute_time_distance(time1, time2):
+    return euclidean(get_time_val(time1), get_time_val(time2))
+
+def get_time_val(time):
+    if time == 'late':
+        return '0.0'
+    if time == 'morning':
+        return '1.0'
+    if time == 'afternoon':
+        return '2.0'
+    if time == 'evening':
+        return '3.0'
+
+def euclidean(this_x, this_y):
+    dec_x = Decimal(this_x)
+    dec_y = Decimal(this_y)
+    return numpy.sqrt(dec_x**2 + dec_y**2)
+
+# array to hold all the instances in this dataset
+instances = []
+
+
+distance_matrix = numpy.zeros(shape=(2354, 2354))
+
+with open('twitter_users_12.csv', 'rb') as my_file:
+    csv_reader = csv.reader(my_file)
+    for row in csv_reader:
+        instances.append(row)
+
+instances_size = len(instances)
+distance_matrix = numpy.zeros(shape=(instances_size, instances_size))
+# Populate the distance matrix by filling up rows at a time
+for x in range(instances_size):
+    for y in range(instances_size):
+        print "Working on cell {px},{py}".format(px=x, py=y)
+        if x == y:
+            distance_matrix[x][y] = 0.0
+        else:
+            if distance_matrix[y][x] != 0.0:
+                # avoid recalculating
+                distance_matrix[x][y] = distance_matrix[y][x]
+            else:
+                distance_matrix[x][y] = compute_distance(instances[x], instances[y])
+
+# TODO write it out to a file
+print "Done"
+
+
